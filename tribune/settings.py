708ddf439e61a342d7d20b50cfb3d1150f509f44
DjCooGie <evansonchagwa01@gmail.com>
2020-01-28 16:22:58 +0300

    configure image upload settings

--- tribune/settings.py
+++ tribune/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -82,9 +83,9 @@ DATABASES = {
         'NAME':'tribune',
         'USER':'moringa',
         'PASSWORD':'hahaa',
-        'TEST': {
-            'NAME': 'test_tribune',
-        },
+        # 'TEST': {
+        #     'NAME': 'test_tribune',
+        # },
     }
 }
 
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
