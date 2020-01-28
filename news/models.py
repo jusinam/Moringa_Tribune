124f940fabef17d892f17b4d04a192f544a48dad
DjCooGie <evansonchagwa01@gmail.com>
2020-01-28 12:20:20 +0300

    add title to landing template and update models and views

--- news/models.py
+++ news/models.py
@@ -1,9 +1,11 @@
+import datetime as dt
 from django.db import models
 
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
         return self.first_name
@@ -23,3 +25,15 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
