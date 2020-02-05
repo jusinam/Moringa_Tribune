adc9e89794eb4e1eb1bae0adda77ea8fca545aa7
DjCooGie <evansonchagwa01@gmail.com>
2020-02-05 15:12:37 +0300

    create news recipient instance the news_of_day view function

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
+from .forms import NewsLetterForm
+from .email import send_welcome_email
 from .models import Article
 from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 
 # Creating views
@@ -10,8 +12,22 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today_news.html', {"date": date,"news":news})
 
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_of_day')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today_news.html', {"date": date,"news":news,"letterForm":form})
 
 
 # View Function to present news from past days
@@ -28,7 +44,7 @@ def past_days_news(request, past_date):
         return redirect(news_today)
 
     news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+    return render(request, 'all-news/past_news.html',{"date": date,"news":news})
 
 
 def search_results(request):
