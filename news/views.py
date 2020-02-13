bba4c1f531c8baf947342d5a4ddf5cf6578659e1
DjCooGie <evansonchagwa01@gmail.com>
2020-02-13 16:22:58 +0300

    create post function int MerchList view class

--- news/views.py
+++ news/views.py
@@ -106,4 +106,9 @@ class MerchList(APIView):
         serializers = MerchSerializer(all_merch, many=True)
         return Response(serializers.data)
 
-        
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
